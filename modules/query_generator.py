bab7b89d625655f697ca9990dd11c68ad2b96f0b
Nick <nikolaszeigler@gmail.com>
2023-08-02 18:00:42 -0700

    added sql args

--- modules/query_generator.py
+++ modules/query_generator.py
@@ -1,11 +1,12 @@
 # query_generator.py
 from openai_api import generate_response
+from SQL_args import sql_commands
 import json
 
 def generate_sql_query(schema, query, command):
     system_message = f'''
-    You are an SQL query generator. Generate a {command.upper()} query based on the following schema and user's input.
-    Schema: ''' + json.dumps(schema, indent=4)
+    You are an SQL query generator. Generate a {command.upper()} query based on the following schema, available args, and the user's input.
+    Schema: ''' + json.dumps(schema, indent=4) + json.dumps(sql_commands, indent=4)
 
     messages=[
         {"role": "system", "content": system_message},
